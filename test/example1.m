210aa19553a6464e5cedb578bfd84b1880a9f46b
Zhen Ji <zhen_ji@hotmail.com>
2021-04-08 16:34:40 -0500

    Start

---
+++ test/example1.m
@@ -0,0 +1,90 @@
+clearvars
+
+imagesDir = 'X:\Lab\Zhen\SRDL';
+% url = 'http://www-i6.informatik.rwth-aachen.de/imageclef/resources/iaprtc12.tgz';
+% downloadIAPRTC12Data(url,imagesDir);
+
+trainImagesDir = fullfile(imagesDir,'iaprtc12','images','02');
+exts = {'.jpg','.bmp','.png'};
+pristineImages = imageDatastore(trainImagesDir,'FileExtensions',exts);
+
+numel(pristineImages.Files);
+
+upsampledDirName = [trainImagesDir filesep 'upsampledImages'];
+residualDirName = [trainImagesDir filesep 'residualImages'];
+
+scaleFactors = [2 3 4];
+createVDSRTrainingSet(pristineImages,scaleFactors,upsampledDirName,residualDirName);
+
+upsampledImages = imageDatastore(upsampledDirName,'FileExtensions','.mat','ReadFcn',@matRead);
+residualImages = imageDatastore(residualDirName,'FileExtensions','.mat','ReadFcn',@matRead);
+
+augmenter = imageDataAugmenter( ...
+    'RandRotation',@()randi([0,1],1)*90, ...
+    'RandXReflection',true);
+
+patchSize = [41 41];
+patchesPerImage = 64;
+dsTrain = randomPatchExtractionDatastore(upsampledImages,residualImages,patchSize, ...
+    "DataAugmentation",augmenter,"PatchesPerImage",patchesPerImage);
+
+inputBatch = preview(dsTrain);
+disp(inputBatch)
+
+networkDepth = 20;
+firstLayer = imageInputLayer([41 41 1],'Name','InputLayer','Normalization','none');
+% The image input layer is followed by a 2-D convolutional layer that contains 64 filters of size 3-by-3. The mini-batch size determines the number of filters. Zero-pad the inputs to each convolutional layer so that the feature maps remain the same size as the input after each convolution. He's method [3] initializes the weights to random values so that there is asymmetry in neuron learning. Each convolutional layer is followed by a ReLU layer, which introduces nonlinearity in the network.
+
+convLayer = convolution2dLayer(3,64,'Padding',1, ...
+    'WeightsInitializer','he','BiasInitializer','zeros','Name','Conv1');
+
+% Specify a ReLU layer.
+relLayer = reluLayer('Name','ReLU1');
+
+middleLayers = [convLayer relLayer];
+for layerNumber = 2:networkDepth-1
+    convLayer = convolution2dLayer(3,64,'Padding',[1 1], ...
+        'WeightsInitializer','he','BiasInitializer','zeros', ...
+        'Name',['Conv' num2str(layerNumber)]);
+    
+    relLayer = reluLayer('Name',['ReLU' num2str(layerNumber)]);
+    middleLayers = [middleLayers convLayer relLayer];    
+end
+
+% The penultimate layer is a convolutional layer with a single filter of size 3-by-3-by-64 that reconstructs the image.
+convLayer = convolution2dLayer(3,1,'Padding',[1 1], ...
+    'WeightsInitializer','he','BiasInitializer','zeros', ...
+    'NumChannels',64,'Name',['Conv' num2str(networkDepth)]);
+% The last layer is a regression layer instead of a ReLU layer. The regression layer computes the mean-squared error between the residual image and network prediction.
+
+finalLayers = [convLayer regressionLayer('Name','FinalRegressionLayer')];
+
+% Concatenate all the layers to form the VDSR network.
+layers = [firstLayer middleLayers finalLayers];
+
+
+maxEpochs = 100;
+epochIntervals = 1;
+initLearningRate = 0.1;
+learningRateFactor = 0.1;
+l2reg = 0.0001;
+miniBatchSize = 64;
+options = trainingOptions('sgdm', ...
+    'Momentum',0.9, ...
+    'InitialLearnRate',initLearningRate, ...
+    'LearnRateSchedule','piecewise', ...
+    'LearnRateDropPeriod',10, ...
+    'LearnRateDropFactor',learningRateFactor, ...
+    'L2Regularization',l2reg, ...
+    'MaxEpochs',maxEpochs, ...
+    'MiniBatchSize',miniBatchSize, ...
+    'GradientThresholdMethod','l2norm', ...
+    'GradientThreshold',0.01, ...
+    'Plots','training-progress', ...
+    'Verbose',false);
+
+    modelDateTime = datestr(now,'dd-mmm-yyyy-HH-MM-SS');
+    tic
+    net = trainNetwork(dsTrain,layers,options);
+    toc
+    save(['trainedVDSR-' modelDateTime '-Epoch-' num2str(maxEpochs*epochIntervals) '-ScaleFactors-' num2str(234) '.mat'],'net','options');
